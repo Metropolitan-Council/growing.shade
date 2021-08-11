1b66e3e9d12e5e0905b0fe40d0a0877ad1246e89
Ellen Esch <Ellen.Esch@metc.state.mn.us>
2021-08-11 17:28:43 -0500

    Update otherdata_holc.R

--- data-raw/otherdata_holc.R
+++ data-raw/otherdata_holc.R
@@ -28,3 +28,16 @@ fs::file_delete("plan_historic_holc_appraisal.gpkg")
 # levels(as.factor(redline$HSG_SCALE))
 # redline %>% ggplot()+geom_sf()
 usethis::use_data(redline, overwrite = TRUE)
+
+
+## transit routes ---------------
+temp <- tempfile()
+download.file("ftp://ftp.gisdata.mn.gov/pub/gdrs/data/pub/us_mn_state_metc/trans_transit_routes/gpkg_trans_transit_routes.zip",
+  destfile = temp
+)
+trans_routes <- sf::read_sf(unzip(temp, "trans_transit_routes.gpkg")) %>%
+  st_transform(4326)
+
+fs::file_delete("trans_transit_routes.gpkg")
+
+usethis::use_data(trans_routes, overwrite = TRUE)
