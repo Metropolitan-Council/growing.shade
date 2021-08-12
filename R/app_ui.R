a8a751b99f4f4b961869708f2fa1e029a0c285c2
Ellen Esch <Ellen.Esch@metc.state.mn.us>
2021-08-11 21:28:25 -0500

    streamline text

--- R/app_ui.R
+++ R/app_ui.R
@@ -38,15 +38,16 @@ app_ui <- function(request) {
                
                fluidRow(
                  mod_preset_selections_ui("preset_selections_ui_1"),
-                 # h3("Step 1: Select priority variables"), p("Select a preset or 'custom' variables and click 'update map.' Resulting values for each tract ranges from 0-10, and represents an average of standardized and scaled raw values."), 
                  # br(),
                  mod_map_selections_ui("map_selections_ui_1")), hr(),
                fluidRow(
-                 column(width = 6, h3("Step 2: View region-wide priority areas"), p("Warm and bright (yellow, orange) values and high ranks correspond to ‘opportunity zones’ where new tree plantings could have disproportionately positive impacts (values closer to 10). Cool and dark colors (black, purple) correspond to lower opportunity areas (values closer to zero)."), br(), mod_map_overview_ui("map_overview_ui_1")),
-                 column(width = 6, h3("Step 3: Detailed priority areas"), p("Click on any area in Step 2 to see more detailed spatial locations which are in need of greening and may be suitable for tree planting. Darker red colors (and NDVI values closer to zero) indicate greater need for greening. No commerical or industrial land uses are shown (but can change!)."), br(), mod_ndvi_map_ui("ndvi_map_ui_1"))),
+                 column(width = 6, h3("Step 2: Region-wide priority areas"), p("Warm and bright (yellow, orange) colors show to ‘opportunity zones’ where new tree plantings could have disproportionately positive impacts (values closer to 10). "), br(), mod_map_overview_ui("map_overview_ui_1")),
+                 column(width = 6, h3("Step 3: Detailed priority areas"), p("Click on an area in Step 2 for detailed locations which may be suitable for tree planting. Darker red colors (and NDVI values closer to zero) indicate greater need for greening."), br(), mod_ndvi_map_ui("ndvi_map_ui_1"))),
                br(), br(),
                
                h2("Make a report"), p("is it too much clutter if we keep adding text here")
+               # downloadButton(outputId = ns("download_button"), label = "Download a report for the selected tract above"),
+               # downloadButton(outputId = ns("download_button2"), label = "Download a report for a city")
                ),
                
                
