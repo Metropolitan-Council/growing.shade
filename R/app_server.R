aeab3f2613b7e263575f8e41b7a9050f13fd0608
Ellen Esch <Ellen.Esch@metc.state.mn.us>
2021-09-29 16:31:45 -0500

    click by ctu

--- R/app_server.R
+++ R/app_server.R
@@ -33,7 +33,8 @@ app_server <- function( input, output, session ) {
   observe({print(paste0("variables used: ", map_selections$allInputs))}) #to check that selections are working
   
   map_util <- callModule(mod_map_utils_server, "map_utils_ui_1",
-                         map_selections = map_selections)
+                         map_selections = map_selections,
+                         geo_selections = geo_selections)
   
   # observe({print((map_util$map_data2))}) #to check that data summary is working
   # observe({print((map_util$plot_data2))}) #to check that plot summary is working
@@ -44,7 +45,8 @@ app_server <- function( input, output, session ) {
              map_util = map_util,
              current_tab = input$nav)
   observe({print(paste0("selected tract: ", (tract_selections$selected_tract)))}) #to check that selections are working
-
+  observe({print(paste0("selected tract: ", (tract_selections$clicked_geo)))}) #to check that selections are working
+  
     # callModule(
   #   mod_main_leaflet_server,
   #   "main_leaflet_ui_1",
@@ -53,21 +55,21 @@ app_server <- function( input, output, session ) {
   #   current_tab = input$nav
   # )
   
-  callModule(mod_ndvi_map_server, "ndvi_map_ui_1",
-             tract_selections = tract_selections)
-  
-  callModule(mod_plot_tract_server, "plot_tract_ui_1",
-             tract_selections = tract_selections,
-             map_util = map_util,
-             map_selections = map_selections)
-  
-  callModule(mod_table_server, "table_ui_1",
-             tract_selections = tract_selections,
-             map_util = map_util)
-  
-  callModule(mod_biodiversity_server, "biodiversity_ui_1")
-  
-  callModule(mod_next_server, "next_ui_1")
+  # callModule(mod_ndvi_map_server, "ndvi_map_ui_1",
+  #            tract_selections = tract_selections)
+  # 
+  # callModule(mod_plot_tract_server, "plot_tract_ui_1",
+  #            tract_selections = tract_selections,
+  #            map_util = map_util,
+  #            map_selections = map_selections)
+  # 
+  # callModule(mod_table_server, "table_ui_1",
+  #            tract_selections = tract_selections,
+  #            map_util = map_util)
+  # 
+  # callModule(mod_biodiversity_server, "biodiversity_ui_1")
+  # 
+  # callModule(mod_next_server, "next_ui_1")
   
   
   }
