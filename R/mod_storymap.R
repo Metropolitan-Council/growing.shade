0d5dc027d37478d5ada2d1da91f6ed29e62c4cb4
Ellen Esch <Ellen.Esch@metc.state.mn.us>
2021-12-29 13:14:39 -0600

    Update mod_storymap.R

--- R/mod_storymap.R
+++ R/mod_storymap.R
@@ -10,23 +10,7 @@
 mod_storymap_ui <- function(id) {
   ns <- NS(id)
   tagList(
-    # uiOutput(ns("storymap"))
-    # htmlOutput("storymap")
-    # tags$iframe(src="https://storymaps.arcgis.com/stories/e61c8e0e54e24485b956601fdc80b63e", style='width:100%; margin:0;padding:0;height:80vh;border:none')#,
-
     htmlOutput(ns("storymap"))
-
-
-
-
-    # width:100%
-    # HTML('<center>
-    #      <section style="position:absolute; height:100%; width:100%; border:none;"> <iframe src="https://storymaps.arcgis.com/stories/e61c8e0e54e24485b956601fdc80b63e"></iframe></section></center>'),
-    #
-    # br(),
-    #
-    # p("We can just have a nice hyperlink to the storymap link"),
-    # HTML('<center><iframe width="560" height="500" src="https://metrocouncil.maps.arcgis.com/apps/MapJournal/index.html?appid=7d9cdd3929e9439bb5b25aa1186d5783"></iframe></center>')
   )
 }
 
@@ -37,13 +21,8 @@ mod_storymap_server <- function(id) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
 
-    # output$storymap <- renderUI({
-    #   tags$iframe(src="https://storymaps.arcgis.com/stories/e61c8e0e54e24485b956601fdc80b63e", style='width:100%; margin:0;padding:0;height:80vh;border:none')
-    # })
-
     output$storymap <- renderUI({
       my_test <- HTML("<div style='max-width:3000px'><body><iframe title='StoryMap about the Growing Shade Project' src='https://storymaps.arcgis.com/stories/1a7fe9365a8f4bfc81305bb288fbbcfa' style='width:100%; margin:0 !important;padding:0;height:85vh;border:none; frameborder = '0'</iframe></body></div>")
-      # my_test <- tags$iframe(src= "https://arcgis.metc.state.mn.us/portal/apps/storymaps/stories/f530052fb2494a63984d1062346396bf", style='width:100% !important; margin:0 !important;padding:0;height:80vh;border:none')
       my_test
     })
   })
