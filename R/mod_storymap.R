fdc639320de3fba1511f7a3451caf06e956198cf
Ellen Esch <Ellen.Esch@metc.state.mn.us>
2021-12-30 09:44:54 -0600

    update iframe

--- R/mod_storymap.R
+++ R/mod_storymap.R
@@ -10,6 +10,7 @@
 mod_storymap_ui <- function(id) {
   ns <- NS(id)
   tagList(
+    # tags$style(type = "text/css", "#storymap {overflow:hidden}"),
     htmlOutput(ns("storymap"))
   )
 }
@@ -22,7 +23,13 @@ mod_storymap_server <- function(id) {
     ns <- session$ns
 
     output$storymap <- renderUI({
-      my_test <- HTML("<div style='max-width:3000px'><body><iframe title='StoryMap about the Growing Shade Project' src='https://storymaps.arcgis.com/stories/1a7fe9365a8f4bfc81305bb288fbbcfa' style='width:100%; margin:0 !important;padding:0;height:85vh;border:none; frameborder = '0'</iframe></body></div>")
+      # my_test <- HTML("<div style='max-width:3000px'><body><iframe title='StoryMap about the Growing Shade Project' src='https://storymaps.arcgis.com/stories/1a7fe9365a8f4bfc81305bb288fbbcfa' style='width:100%; top:150px !important; bottom:0 !important;padding:0;height:100vh;border:none; frameborder = '0'</iframe></body></div>")
+      my_test <- HTML("<div style='max-width:3000px;'><body><iframe title='StoryMap about the Growing Shade Project' src='https://storymaps.arcgis.com/stories/1a7fe9365a8f4bfc81305bb288fbbcfa' style='width:100%; height:calc(100vh - 120px); padding:0;border:0; object-position: center bottom;'</iframe></body></div>")
+      
+      # margin vs bottom
+      # height 88vh
+      # calc(100vh - 80px)
+      
       my_test
     })
   })
