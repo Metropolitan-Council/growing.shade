086237132e7442cd2e24b10c5a268ac8739d25b1
Ellen Esch <Ellen.Esch@metc.state.mn.us>
2022-01-13 16:29:03 -0600

    Update app_ui.R

--- R/app_ui.R
+++ R/app_ui.R
@@ -42,7 +42,7 @@ app_ui <- function(request) {
       tabPanel(
         "Mapping tool",
         tags$footer(
-          HTML('Source: <a href = "https://metrotransitmn.shinyapps.io/growing-shade/" target = "_blank">Growing Shade Project</a>. Last updated on 2022-01-11. '),
+          HTML('Source: <a href = "https://metrotransitmn.shinyapps.io/growing-shade/" target = "_blank">Growing Shade Project</a>. Last updated on 2022-01-13. '),
           align = "right",
           style = "
               position:absolute;
