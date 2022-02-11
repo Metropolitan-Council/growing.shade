b02e6d0249e9221b6eb66d5062fe7a4d22135130
ehesch <ellen.esch@metc.state.mn.us>
2022-02-10 19:32:04 -0600

    export more data in dl

--- R/mod_report.R
+++ R/mod_report.R
@@ -1069,7 +1069,11 @@ mod_report_server <- function(id,
                 `Environmental justice priority score` = `Environmental justice`,
                 `Public health priority score` = `Public health`,
                 `Percent tree cover` = canopy_percent
-              ),
+              ) %>%
+              left_join(bg_growingshade_main %>%
+                          select(tract_string, variable, raw_value) %>%
+                          pivot_wider(names_from = variable, values_from = raw_value) %>%
+                          rename(GEO_ID = tract_string), by = c("GEO_ID")),
             "Entire Region" = bg_growingshade_main %>%
               select(tract_string, variable, raw_value) %>%
               pivot_wider(names_from = variable, values_from = raw_value) %>%
