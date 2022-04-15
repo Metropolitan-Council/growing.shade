411a90cb6c05c2aa9745e01457584aefc305b0f3
ehesch <ellen.esch@metc.state.mn.us>
2022-04-15 17:41:21 -0500

    change tree zoom layer

--- R/mod_map_overview.R
+++ R/mod_map_overview.R
@@ -276,11 +276,11 @@ mod_map_overview_server <- function(input, output, session,
         # "Emerald ash borer",
         "Historically redlined areas",
         "Road outlines"
-      )) #%>%
-      # groupOptions(
-      #   group = "Trees",
-      #   zoomLevels = 15:18
-      # )
+      )) %>%
+      groupOptions(
+        group = "Trees",
+        zoomLevels = 13:18
+      )
     # groupOptions(
     #       group = "Road outlines",
     #       zoomLevels = 13:13# 13:18
