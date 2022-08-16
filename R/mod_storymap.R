fd6a84610b11f1dcb2bc617537afd85ebb62b13b
ehesch <ellen.esch@metc.state.mn.us>
2022-08-16 12:26:42 -0500

    edit storymap for mobile

--- R/mod_storymap.R
+++ R/mod_storymap.R
@@ -36,7 +36,9 @@ mod_storymap_ui <- function(id) {
     # h2("Old Faithful Geyser Data", class="d-none d-lg-block"),
     # h2("Geyser Data", class="d-block d-lg-none"),
       br(class="d-none d-lg-block"),
-    htmlOutput(ns("storymap"))
+      # htmlOutput(ns("storymap_wide"), class = "d-none d-lg-block"),
+      # htmlOutput(ns("storymap_narrow"), class = "d-block d-lg-none")
+      htmlOutput(ns("storymap"))
   )
   )
 }
@@ -50,14 +52,29 @@ mod_storymap_server <- function(id) {
 
     output$storymap <- renderUI({
       # my_test <- HTML("<div style='max-width:3000px'><body><iframe title='StoryMap about the Growing Shade Project' src='https://storymaps.arcgis.com/stories/1a7fe9365a8f4bfc81305bb288fbbcfa' style='width:100%; top:150px !important; bottom:0 !important;padding:0;height:100vh;border:none; frameborder = '0'</iframe></body></div>")
-      my_test <- HTML("<div style='max-width:3000px;'><body><iframe title='StoryMap about the Growing Shade Project' src='https://storymaps.arcgis.com/stories/1a7fe9365a8f4bfc81305bb288fbbcfa' style='width:100%; height:calc(100vh - 120px); padding:0;border:0; object-position: center bottom;'</iframe></body></div>")
+      my_test <- HTML("<div style='max-width:3000px;'><body><iframe title='StoryMap about the Growing Shade Project' src='https://storymaps.arcgis.com/stories/1a7fe9365a8f4bfc81305bb288fbbcfa' style='width:100%; height:calc(88vh); padding:0;border:0; object-position: center bottom;'</iframe></body></div>")
 
       # margin vs bottom
       # height 88vh
-      # calc(100vh - 80px)
+      # calc(100vh - 120px) #generally, this is working well!! but need to use 88vh to make work okay on mobile
 
       my_test
     })
+    
+    
+    # output$storymap_narrow <- renderUI({
+    #   # my_test <- HTML("<div style='max-width:3000px'><body><iframe title='StoryMap about the Growing Shade Project' src='https://storymaps.arcgis.com/stories/1a7fe9365a8f4bfc81305bb288fbbcfa' style='width:100%; top:150px !important; bottom:0 !important;padding:0;height:100vh;border:none; frameborder = '0'</iframe></body></div>")
+    #   my_test <- HTML("<div style='max-width:3000px;'><body><iframe title='StoryMap about the Growing Shade Project' src='https://storymaps.arcgis.com/stories/1a7fe9365a8f4bfc81305bb288fbbcfa' style='width:100%; height:calc(100vh - 120px); padding:0;border:0; object-position: center bottom;'</iframe></body></div>")
+    #   
+    #   # margin vs bottom
+    #   # height 88vh
+    #   # calc(100vh - 80px)
+    #   
+    #   my_test
+    # })
+    
+    
+
   })
 }
 
