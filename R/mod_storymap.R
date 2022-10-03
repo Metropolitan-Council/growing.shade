44090e74600d4921d52c43268f36e8b046f630d7
ehesch <ellen.esch@metc.state.mn.us>
2022-10-03 17:08:18 -0500

    Update mod_storymap.R

--- R/mod_storymap.R
+++ R/mod_storymap.R
@@ -12,17 +12,8 @@ mod_storymap_ui <- function(id) {
 
   
   tagList(
-    # tags$head(tags$style(HTML(hiding_js))),
-    # tags$style(type = "text/css", "#storymap {overflow:hidden}"),
-    # div(br(class = "d-none d-lg-block")),
-    # bootstrapPage(# theme  = bslib::bs_theme(version = 5),
-    # h2("Old Faithful Geyser Data", class="d-none d-lg-block"),
-    # h2("Geyser Data", class="d-block d-lg-none"),
       br(class="d-none d-lg-block"),
-      # htmlOutput(ns("storymap_wide"), class = "d-none d-lg-block"),
-      # htmlOutput(ns("storymap_narrow"), class = "d-block d-lg-none")
       htmlOutput(ns("storymap"))
-  # )
   )
 }
 
@@ -43,21 +34,6 @@ mod_storymap_server <- function(id) {
 
       my_test
     })
-    
-    
-    # output$storymap_narrow <- renderUI({
-    #   # my_test <- HTML("<div style='max-width:3000px'><body><iframe title='StoryMap about the Growing Shade Project' src='https://storymaps.arcgis.com/stories/1a7fe9365a8f4bfc81305bb288fbbcfa' style='width:100%; top:150px !important; bottom:0 !important;padding:0;height:100vh;border:none; frameborder = '0'</iframe></body></div>")
-    #   my_test <- HTML("<div style='max-width:3000px;'><body><iframe title='StoryMap about the Growing Shade Project' src='https://storymaps.arcgis.com/stories/1a7fe9365a8f4bfc81305bb288fbbcfa' style='width:100%; height:calc(100vh - 120px); padding:0;border:0; object-position: center bottom;'</iframe></body></div>")
-    #   
-    #   # margin vs bottom
-    #   # height 88vh
-    #   # calc(100vh - 80px)
-    #   
-    #   my_test
-    # })
-    
-    
-
   })
 }
 
