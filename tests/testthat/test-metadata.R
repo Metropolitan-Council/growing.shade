4f2951b460f54089007e158495906eb77c0904a7
Liz Roten <liz.roten@metc.state.mn.us>
2023-11-02 14:03:34 -0500

    add, update tests

---
+++ tests/testthat/test-metadata.R
@@ -0,0 +1,67 @@
+testthat::test_that("bg_growingshade_main data is valid", {
+
+  metadata %>% 
+    filter(variable == "hhincome") %>% 
+    magrittr::extract2("MEANRAW") %>% 
+    testthat::expect_equal(87845,
+                           tolerance = 0.01)
+
+  metadata %>% 
+    filter(variable == "ppov185") %>% 
+    magrittr::extract2("MEANRAW") %>% 
+    testthat::expect_equal(0.182,
+                           tolerance = 0.01)
+      
+  metadata %>% 
+    filter(variable == "pbipoc") %>% 
+    magrittr::extract2("MEANRAW") %>% 
+    testthat::expect_equal(0.312,
+                           tolerance = 0.01)
+  
+  metadata %>% 
+    filter(variable == "pd_any") %>% 
+    magrittr::extract2("MEANRAW") %>% 
+    testthat::expect_equal(0.0996,
+                           tolerance = 0.01)
+  
+  
+  metadata %>% 
+    filter(variable == "pownhome") %>% 
+    magrittr::extract2("MEANRAW") %>% 
+    testthat::expect_equal(0.691,
+                           tolerance = 0.01)
+  
+  
+  metadata %>% 
+    filter(variable == "CASTHMA") %>% 
+    magrittr::extract2("MEANRAW") %>% 
+    testthat::expect_equal(0.0945,
+                           tolerance = 0.01)
+  
+  
+  metadata %>% 
+    filter(variable == "MHLTH") %>% 
+    magrittr::extract2("MEANRAW") %>% 
+    testthat::expect_equal(0.14,
+                           tolerance = 0.01)
+  
+  
+  metadata %>% 
+    filter(variable == "canopy_percent") %>% 
+    magrittr::extract2("MEANRAW") %>% 
+    testthat::expect_equal(0.423,
+                           tolerance = 0.01)
+  
+  
+  metadata %>% 
+    filter(variable == "pownhome") %>% 
+    magrittr::extract2("MEANRAW") %>% 
+    testthat::expect_equal(0.691,
+                           tolerance = 0.01)
+  
+  metadata %>% 
+    magrittr::extract2("n") %>% 
+    unique() %>% 
+    testthat::expect_equal(2586)
+  
+})
